f9ca348f3a8315cd9efcd6a37b9a7b0c428f16d9
opilane <opilane@HP-CZC6297B8B>
2021-11-09 09:17:12 +0200

    add home controller test

---
+++ OdeToFood test/Controllers/HomeControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OdeToFood.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OdeToFood_test.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {   
+        [TestMethod]
+        public void About()
+        {
+            HomeController Controller = new HomeController();
+            ViewResult result = Controller.About() as ViewResult;
+            Assert.IsNotNull(result.Model);
+        }
+
+
+    }
+}
