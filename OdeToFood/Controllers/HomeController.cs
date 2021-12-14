fb44571941b2b25e46abf899555475307f6e7916
opilane <opilane@HP-CZC6297B8B>
2021-12-14 14:07:04 +0200

    minor fixes to links and review creation

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
-﻿using AspNetCore.Unobtrusive.Ajax;
+﻿using Microsoft.AspNetCore.Mvc;
+using AspNetCore.Unobtrusive.Ajax;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using OdeToFood.Data;
 using OdeToFood.Models;
@@ -9,22 +9,21 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-
+using X.PagedList;
+using X.PagedList.Mvc;
 namespace OdeToFood.Controllers
 {
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
-
 		private ApplicationDbContext _context;
-        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
-        {
-            _logger = logger;
-            _context = context;
-        }
-
-		public IActionResult  Autocomplete(string term)
-        {
+		public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
+		{
+			_logger = logger;
+			_context = context;
+		}
+		public ActionResult Autocomplete(string term)
+		{
 			var model = _context.Restaurants
 				.Where(r => r.Name.StartsWith(term))
 				.Take(10)
@@ -33,31 +32,29 @@ namespace OdeToFood.Controllers
 					label = r.Name
 				});
 			return Json(model);
-        }
-
-		public IActionResult Index(string searchTerm = null)
+		}
+		public IActionResult Index(string searchTerm = null, int page = 1)
 		{
-
 			var model = _context.Restaurants
-				.OrderByDescending(r=>r.Reviews.Average(review => review.Rating)
-				)
-				.Where(r=>searchTerm==null || r.Name.StartsWith(searchTerm))
+				.OrderByDescending(
+					r => r.Reviews.Average(review => review.Rating)
+					)
+				.Where(r => searchTerm == null || r.Name.Contains(searchTerm))
 				.Select(r => new RestaurantListViewModel
-                {
-                    Id = r.Id,
-                    Name = r.Name,
-                    Country = r.Country,
-                    City = r.City,
-                    CountOfReviews = r.Reviews.Count
+				{
+					Id = r.Id,
+					Name = r.Name,
+					City = r.City,
+					Country = r.Country,
+					CountOfReviews = r.Reviews.Count
+				}).ToPagedList(page, 10);
 
-                });
-
-            if (Request.IsAjaxRequest())
-            {
+			if (Request.IsAjaxRequest())
+			{
 				return PartialView("_Restaurants", model);
-            }
+			}
 
-            return View(model);
+			return View(model);
 		}
 		public IActionResult About()
 		{
@@ -72,13 +69,12 @@ namespace OdeToFood.Controllers
 		{
 			return View();
 		}
-
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 		}
 
-     
-    }
+
+	}
 }
